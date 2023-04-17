941084ca3d5c1efaa9afbd01118f26fc507fe8bd
SashaTolkodubova <atolkodubova@gmail.com>
2023-04-18 00:52:28 +0200

    like library

---
+++ gendiff/__init__.py
@@ -0,0 +1,5 @@
+from gendiff.gendiff.gendiff import generate_diff
+
+NAME = 'gendiff'
+
+__all__ = ['generate_diff']
--- gendiff/gendiff/__init__.py
+++
@@ -1,5 +0,0 @@
-from gendiff.gendiff.gendiff import generate_diff
-
-NAME = 'gendiff'
-
-__all__ = ['generate_diff']
