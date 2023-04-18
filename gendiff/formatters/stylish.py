662317c8a9638bc2980aba79051aad8adea4839a
SashaTolkodubova <atolkodubova@gmail.com>
2023-04-18 19:31:09 +0200

    finish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -14,8 +14,8 @@ def stylish(doc, level=0):
 
     def is_value(value):
         if value or value == 0:
-            return f"xxx {value}"
-        return ''
+            return f" {value}"
+        return ' '
 
     def returner(returner_doc, returner_level):
         returner_doc = sorted(returner_doc, key=lambda x: x['key'])
