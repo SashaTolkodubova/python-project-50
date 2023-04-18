b5614d607b4dc270667c06cd0fcd5e99475f4414
SashaTolkodubova <atolkodubova@gmail.com>
2023-04-18 19:03:29 +0200

    finish

--- tests/test_to_json.py
+++ tests/test_to_json.py
@@ -9,8 +9,8 @@ def test_to_json():
     file2_path = 'tests/fixtures/file2.json'
     file1, file2 = parsing(file1_path, file2_path)
     answer_path = 'tests/fixtures/json_unswer.json'
+    test_answer = to_json(diff(file1, file2))
     with open(answer_path, 'r') as file1_json:
         answer = json.load(file1_json)
-    test_answer = to_json(diff(file1, file2))
     test_answer = json.loads(test_answer)
     assert test_answer == answer
