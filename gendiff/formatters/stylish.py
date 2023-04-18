f0593ef3da2957eeaa29b006247696457d884468
SashaTolkodubova <atolkodubova@gmail.com>
2023-04-18 20:32:36 +0200

    finish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -30,10 +30,10 @@ def stylish(doc, level=0):
             else:
                 if i['value'] == '':
                     result += f"\n{'    ' * returner_level}  " \
-                              f"{i['tag']} {i['key']}:"
+                              f"{i['tag']} {i['key']}: "
                 else:
                     result += f"\n{'    ' * returner_level}  " \
-                              f"{i['tag']} {i['key']}: x{is_value(i['value'])}"
+                              f"{i['tag']} {i['key']}: {is_value(i['value'])}"
         result += f"\n{'    ' * returner_level}{'}'}"
         return result
     return returner(doc, level)
